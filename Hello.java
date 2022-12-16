3430aca08f97f3b9e8cdf6879e5a5abae296fb8d
musfiqur172 <musfiqur.rahman172@gmail.com>
2022-12-16 16:15:06 +0600

    first commit

---
+++ Hello.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int r = sc.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int c = sc.nextInt();
+
+        int[][] array = new int[10][10];
+
+        System.out.println("Enter the elements of the array: ");
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                array[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Array elements are: ");
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ inputProgram.java
@@ -0,0 +1,5 @@
+public class inputProgram {
+    public static void main(String[] args) {
+
+    }
+}
